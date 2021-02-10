0265a47980d08a04904e5366eb3124b94d9f24e2
Xavier Everaert <xeveraert@gmail.com>
2021-02-10 16:22:26 +0100

    all crud actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,19 +10,37 @@ class EmployeesController < ApplicationController
   end
 
   def new
+    @employee = Employee.new
   end
 
   def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+    redirect_to employee_path(@employee)
+
   end
 
   def edit
+    @employee = Employee.find(params[:id])
   end
 
   def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    redirect_to employee_path(@employee)
   end
 
   def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to employees_path
   end
+
+private
+
+def employee_params
+  params.require(:employee).permit(:name, :role)
+end
 
 end
 
